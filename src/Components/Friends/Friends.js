e58dccf73ba3f22edd9f9cc5229e3fa660bdb537
laskor <al.moksud@gmail.com>
2022-03-29 19:12:57 -0400

    api added

--- src/Components/Friends/Friends.js
+++ src/Components/Friends/Friends.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import './Friends.css';
+import React, { useEffect, useState } from "react";
+import "./Friends.css";
 
 const Friends = () => {
-    return (
-        <div>
-            <h1>Friends</h1>
-        </div>
-    );
+  const [friends, setFriends] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => setFriends(data));
+  }, []);
+  return (
+    <div>
+      <h1>Friends</h1>
+    </div>
+  );
 };
 
 export default Friends;
