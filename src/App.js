08e0cde8e2ec2489a65dea00762c975679af0b93
laskor <al.moksud@gmail.com>
2022-03-29 18:50:45 -0400

    components added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './Components/Home/Home';
+import Friends from './Components/Friends/Friends';
+import About from './Components/About/About';
 
 function App() {
   return (
     <div className="App">
-      
+      <Home></Home>
+      <Friends></Friends>
+      <About></About>
     </div>
   );
 }
