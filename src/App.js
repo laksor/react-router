133bbb55084d3df85af0068f76cfc271d45cda24
laskor <al.moksud@gmail.com>
2022-03-29 19:10:03 -0400

    route added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Components/Home/Home';
 import Friends from './Components/Friends/Friends';
 import About from './Components/About/About';
+import NotFound from './Components/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
-      <Friends></Friends>
-      <About></About>
+      <h1>Welcome to my Routing Display</h1>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path='friends' element={<Friends />}></Route>
+        <Route path='about' element={<About />}></Route>
+        <Route path='*' element={<NotFound />}></Route>
+      </Routes>
     </div>
   );
 }
