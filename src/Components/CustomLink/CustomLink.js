e94c1d19bff1110d7f4f5f2991ea9de25e6a0e7f
laskor <al.moksud@gmail.com>
2022-03-30 22:16:45 -0400

    custom link

--- src/Components/CustomLink/CustomLink.js
+++ src/Components/CustomLink/CustomLink.js
@@ -8,7 +8,7 @@ function CustomLink({ children, to, ...props }) {
     return (
       <div>
         <Link
-          style={{color: match ? 'red' : 'black' ,textDecoration: match ? "underline" : "none" }}
+          style={{color: match ? 'red' : 'black' ,textDecoration: "none" }}
           to={to}
           {...props}
         >
